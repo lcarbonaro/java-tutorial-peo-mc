e78d04c68509f06513679937a9a6fcd92a939b38
Les Carbonaro <lcarbonaro@gmail.com>
2023-10-07 15:21:24 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import 'reveal.js/dist/reset.css';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
+import 'reveal.js/plugin/highlight/monokai.css';
+
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm';
+import Zoom from 'reveal.js/plugin/zoom/zoom.esm';
+import Notes from 'reveal.js/plugin/notes/notes.esm';
+import Search from 'reveal.js/plugin/search/search.esm';
+
+/*
+<script src="plugin/zoom/zoom.js"></script>
+	<script src="plugin/notes/notes.js"></script>
+	<script src="plugin/search/search.js"></script>
+*/
+
+let deck = new Reveal({
+   plugins: [ Markdown,Highlight,Zoom,Notes,Search ]
+})
+deck.initialize();
